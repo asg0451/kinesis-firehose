385e75e2b0fca980585f11a4c6b4157b975a6d31
Miles Frankel <miles.frankel@gmail.com>
2021-04-03 15:25:34 -0400

    remove inner shit in async prod

--- kinesis-firehose-producer/src/async_producer.rs
+++ kinesis-firehose-producer/src/async_producer.rs
@@ -6,26 +6,18 @@ use crate::buffer::Buffer;
 use crate::error::Error;
 use crate::put_record_batcher::PutRecordBatcher;
 
-// this split is solely so producer.drop can work
-// but we're no longer doing that, so they can be collapsed again
-// alternatively, inner.is_none could -> closed.
-// TODO
-struct InnerProducer<C: PutRecordBatcher> {
+pub struct Producer<C: PutRecordBatcher> {
     buf: Buffer,
     client: C,
     stream_name: String,
 }
 
-pub struct Producer<C: PutRecordBatcher> {
-    inner: Option<InnerProducer<C>>,
-}
-
 pub type KinesisFirehoseProducer = Producer<KinesisFirehoseClient>;
 
-impl<C: PutRecordBatcher> InnerProducer<C> {
+impl<C: PutRecordBatcher> Producer<C> {
     // this WILL add newlines. don't add them yourself
     #[throws]
-    async fn produce(&mut self, mut rec: String) {
+    pub async fn produce(&mut self, mut rec: String) {
         rec += "\n";
         self.buf.check_record_too_large(&rec)?;
 
@@ -38,7 +30,7 @@ impl<C: PutRecordBatcher> InnerProducer<C> {
     // due to a bug in fehler (fixed on master but not in release) this warns unreachable_code
     #[allow(unreachable_code)]
     #[throws]
-    async fn flush(&mut self) {
+    pub async fn flush(&mut self) {
         if self.buf.is_empty() {
             return;
         }
@@ -91,92 +83,28 @@ impl<C: PutRecordBatcher> InnerProducer<C> {
     }
 
     #[throws]
-    async fn close(&mut self) {
+    pub async fn close(&mut self) {
         // TODO: set self.closed, error to produce on it
         self.flush().await?
     }
-}
 
-// this is 'static cause of a compiler error ofc. but is it a bad thing?
-// we're only generic over the type of the rusoto client, and that can live forever i guess...?
-// therefore we dont have Drop flush on this async producer
-// therefore we dont need the 'static lifetime on C
-impl<C: PutRecordBatcher> Producer<C> {
     #[throws]
     pub fn with_client(client: C, stream_name: String) -> Self {
         Self {
-            inner: Some(InnerProducer {
-                buf: Buffer::new(),
-                client,
-                stream_name,
-            }),
+            buf: Buffer::new(),
+            client,
+            stream_name,
         }
     }
-
-    fn inner_mut(&mut self) -> &mut InnerProducer<C> {
-        let ret = self
-            .inner
-            .as_mut()
-            .expect("unexpectedly don't have an InnerProducer");
-        ret
-    }
-
-    #[throws]
-    pub async fn produce(&mut self, rec: String) {
-        self.inner_mut().produce(rec).await?
-    }
-
-    #[throws]
-    pub async fn flush(&mut self) {
-        self.inner_mut().flush().await?
-    }
-
-    #[throws]
-    pub async fn close(&mut self) {
-        // TODO: set self.closed, error to produce on it
-        self.inner_mut().close().await?
-    }
-
-    // NOTE: in a single-threaded tokio runtime this just hangs forever as the main thread is blocked
-    // in rx.recv. therefore don't use this!!
-    // fn sync_close(&mut self) {
-    //     use std::sync::mpsc::channel;
-
-    //     println!("sync_close");
-    //     let (tx, rx) = channel();
-
-    //     let mut inner = self
-    //         .inner
-    //         .take()
-    //         .expect("failed to take InnerProducer in sync_close");
-
-    //     println!("sync_close has inner");
-
-    //     tokio::spawn(async move {
-    //         println!("sync_close spawned");
-    //         let res = inner.close().await;
-    //         println!("sync_close spawned awaited");
-    //         if let Err(err) = res {
-    //             log::warn!("error synchronously closing: {}", err);
-    //         }
-    //         tx.send(()).expect("failed to send on conf channel");
-    //     });
-    //     println!("sync_close about to recv ");
-
-    //     rx.recv().expect("failed to recv on conf channel");
-    //     println!("/sync_close");
-    // }
 }
 
 impl Producer<KinesisFirehoseClient> {
     #[throws]
     pub fn new(stream_name: String) -> Self {
         Self {
-            inner: Some(InnerProducer {
-                buf: Buffer::new(),
-                client: KinesisFirehoseClient::new(Region::default()),
-                stream_name,
-            }),
+            buf: Buffer::new(),
+            client: KinesisFirehoseClient::new(Region::default()),
+            stream_name,
         }
     }
 }
